5ea23e53fd5cefdc9049ada0bd5f90de0b10b7cb
princedanson <princedanson@outlook.com>
2023-06-21 14:30:53 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,380 @@
+#include <Arduino.h>
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <SPI.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+#include  <WiFiClientSecureBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiManager.h>
+#include <WiFiClientSecure.h>
+
+
+#define  RST D1
+#define SS_PIN D2
+#define SIZE 15  
+#define PORT 80
+
+
+
+MFRC522 mfrc522(SS_PIN,RST);
+ESP8266WiFiMulti WiFiMulti;
+WiFiManager wifi;
+void strclean (unsigned char* src);
+
+
+typedef unsigned char byte;
+typedef struct {
+  byte fname[SIZE];
+  byte sname[SIZE];
+  byte lname[SIZE];
+  byte account[SIZE];
+  byte security[SIZE];
+  byte exp_month[3];
+  byte exp_year[SIZE];
+
+}id;
+char address[]="http://paywave-dev.eba-ypxxxpkf.us-east-1.elasticbeanstalk.com/WeatherForecast";
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();
+  mfrc522.PCD_Init();
+
+   for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("Aitel 4G MiFi_65DB", "51377932");
+  Serial.println("Reading Data from Card");
+ 
+
+}
+
+void loop() {
+
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+    
+
+      
+      MFRC522::MIFARE_Key key;
+    
+      HTTPClient https;
+
+
+      for (byte i = 0; i < 6; i++)
+      {
+        key.keyByte[i]= 0xFF;
+      
+      }
+
+      if(!mfrc522.PICC_IsNewCardPresent())
+      {
+        return;
+      }
+      if(!mfrc522.PICC_ReadCardSerial())
+      {
+        return;
+      }
+      Serial.println("Card Detected");
+      // Authentecating block 1
+
+      //block 1
+      byte block ;
+      byte len =18;
+      block = 1;
+      id *person = new id;
+      byte fname[20];
+      MFRC522::StatusCode status;
+
+      status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,block,&key,&(mfrc522.uid));
+      if(status != MFRC522::STATUS_OK)
+      {
+        Serial.printf("Cant Authenticate Block,%d",block);
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+        Serial.printf("Authentication for block %d Successfull\n",block);
+      
+      status = mfrc522.MIFARE_Read(block,fname,&len);
+      memcpy(person->fname,fname,sizeof(fname));
+      if (status != MFRC522::STATUS_OK)
+      {
+        Serial.print("Can't Read Card: ");
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+        Serial.printf("Card Read %s\n\n",person->fname);
+      
+      //block 2 fname
+
+      block=2;
+      byte sname[20];
+      status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,block,&key,&(mfrc522.uid));
+      if(status != MFRC522::STATUS_OK)
+      {
+        Serial.printf("Cant Authenticate Block,%d",block);
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+      {
+        Serial.printf("Authentication for block %d Successfull\n",block);
+      }
+      status = mfrc522.MIFARE_Read(block,sname,&len);
+      memcpy(person->sname,sname,sizeof(sname));
+      if (status != MFRC522::STATUS_OK)
+      {
+        Serial.print("Can't Read Card: ");
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+      Serial.printf("Card Read %s\n\n",person->sname);
+
+      //Read last name
+        block=4;
+      byte lname[20];
+      status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,block,&key,&(mfrc522.uid));
+      if(status != MFRC522::STATUS_OK)
+      {
+        Serial.printf("Cant Authenticate Block,%d",block);
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+        Serial.printf("Authentication for block %d Successfull\n",block);
+      
+      status = mfrc522.MIFARE_Read(block,lname,&len);
+      memcpy(person->lname,lname,sizeof(lname));
+      if (status != MFRC522::STATUS_OK)
+      {
+        Serial.print("Can't Read Card: ");
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+      Serial.printf("Card Read %s\n\n",person->lname);
+
+      
+      //Account No
+      //Read last name
+      block=5;
+      byte acct[20];
+      status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,block,&key,&(mfrc522.uid));
+      if(status != MFRC522::STATUS_OK)
+      {
+        Serial.printf("Cant Authenticate Block,%d",block);
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+        Serial.printf("Authentication for block %d Successfull\n",block);
+      
+      status = mfrc522.MIFARE_Read(block,acct,&len);
+      memcpy(person->account,acct,sizeof(lname));
+      if (status != MFRC522::STATUS_OK)
+      {
+        Serial.print("Can't Read Card: ");
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+      Serial.printf("Card Read %s\n\n",person->account);
+
+
+      //Security Pin
+      //Read last name
+        block=8;
+      byte sec[19];
+      status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,block,&key,&(mfrc522.uid));
+      if(status != MFRC522::STATUS_OK)
+      {
+        Serial.printf("Cant Authenticate Block,%d",block);
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+        {Serial.printf("Authentication for block %d Successfull\n",block);}
+    
+      status = mfrc522.MIFARE_Read(block,sec,&len);
+      memcpy(person->security,sec,sizeof(sec));
+      if (status != MFRC522::STATUS_OK)
+      {
+        Serial.print("Can't Read Card: ");
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+        Serial.printf("Card Read %s\n\n",person->security);
+
+
+      //expiration date
+      block=10;
+      byte month[20];
+      status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,block,&key,&(mfrc522.uid));
+      if(status != MFRC522::STATUS_OK)
+      {
+        Serial.printf("Cant Authenticate Block,%d",block);
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+      {
+        Serial.printf("Authentication for block %d Successfull\n",block);
+      }
+      status = mfrc522.MIFARE_Read(block,month,&len);
+      memcpy(person->exp_month,month,sizeof(month));
+      if (status != MFRC522::STATUS_OK)
+      {
+        Serial.print("Can't Read Card: ");
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+      Serial.printf("Card Read %s\n\n",person->exp_month);
+
+
+      //Read year
+      block=12;
+      byte year[20];
+      status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,block,&key,&(mfrc522.uid));
+      if(status != MFRC522::STATUS_OK)
+      {
+        Serial.printf("Cant Authenticate Block,%d: ",block);
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+      {
+        Serial.printf("Authentication for block %d Successfull\n",block);
+      }
+      status = mfrc522.MIFARE_Read(block,year,&len);
+      memcpy(person->exp_year,year,sizeof(year));
+      if (status != MFRC522::STATUS_OK)
+      {
+        Serial.print("Can't Read Card: ");
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return;
+      }
+      else
+      {
+        Serial.printf("Card Read %s\n\n",person->exp_year);
+      }
+
+    delay(1000);
+    mfrc522.PICC_HaltA();
+    mfrc522.PCD_StopCrypto1();
+    Serial.println("*****************************************************************");
+    Serial.printf(" User name: %s %s %s\n",person->sname,person->fname,person->lname);
+    Serial.printf(" Account no: %s\n",person->account);
+    Serial.printf(" Secutity Code: %s\n",person->security);
+    Serial.printf(" Expiration Date: %s\\%s \n",person->exp_month,person->exp_year);
+    Serial.println("*****************************************************************");
+    String  name[30];
+    strclean(person->sname);
+    strclean(person->fname);
+    strclean(person->lname);
+    strclean(person->account);
+    strclean(person->security);
+    strclean(person->exp_month);
+    strclean(person->exp_year);
+    strclean(person->exp_month);
+    //Serial.print((const __FlashStringHelper*)person->fname);
+
+    Serial.println("*****************************************************************");
+    Serial.printf(" User name: %s %s %s\n",person->sname,person->fname,person->lname);
+    Serial.printf(" Account no: %s\n",person->account);
+    Serial.printf(" Secutity Code: %s\n",person->security);
+    Serial.printf(" Expiration Date: %s\\%s \n",person->exp_month,person->exp_year);
+    Serial.println("*****************************************************************");
+
+    Serial.println();
+    Serial.println();
+    Serial.println();
+    Serial.println("Serialization");
+    Serial.println("****************************************");
+    //Serial.print( input);
+    StaticJsonDocument<200> into;
+    //sonObject obj = into.as<char>();
+
+    into["sname"].set(person->sname);
+    into["fname"].set(person->fname);
+    into["lname"].set(person->lname);
+    into["account"].set(person->account);
+    into["year"].set(person->exp_year);
+    into["month"].set(person->exp_month);
+    into["security"].set(person->security);
+    byte output2[128];
+
+    serializeJsonPretty(into,output2);
+    Serial.printf("%s",output2);
+    int lenh = measureJsonPretty(into);
+    Serial.println();
+    Serial.println(lenh);
+    
+    //WIfi
+ 
+    //std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+WiFiClient client;
+    //client->setFingerprint(fingerprint_sni_cloudflaressl_com);
+     //client->setInsecure();  
+   
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(client, address)) {
+        // HTTPS
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = https.getString();
+          Serial.println(payload);
+        }
+      } else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+
+      https.end();
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
+      delay(10000);
+        delete person;
+  }
+}
+void strclean (unsigned char * src) {
+    // Run two pointers in parallel.
+
+    
+
+    // Process every source character.
+    
+      unsigned char *dst = src;
+    
+
+    while (*src) {
+        // Only copy (and update destination pointer) if suitable.
+        // Update source pointer always.
+
+        if (isalpha(*src)){ *dst++ = *src;}else if(isdigit(*src)){*dst++ = *src;}
+        src++;
+        
+    }
+
+    // Finalise destination string.
+
+    *dst = '\0';
+}
